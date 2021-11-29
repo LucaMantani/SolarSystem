8dd22a2807deb59f5a5ff4109d5f85685b0cbc59
Luca Mantani <luca.mantani@gmail.com>
2021-11-29 21:13:03 +0000

    Made position and velocity public

--- Planet.py
+++ Planet.py
@@ -5,20 +5,12 @@ class Planet(object):
 
     def __init__(self, mass, position, velocity):
         self._mass = mass
-        self._position = position
-        self._velocity = velocity
+        self.position = position
+        self.velocity = velocity
 
     @property
     def mass(self):
         return self._mass
-    
-    @property
-    def position(self):
-        return self._position
-
-    @property
-    def velocity(self):
-        return self._velocity
 
     def __str__(self):
         return "Mass = %f\nPosition = (%f, %f)\nVelocity = (%f, %f)\n"\
