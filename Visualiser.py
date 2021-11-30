237a071a01afe088ce8efc4441c1b4d513ef8bd8
Luca Mantani <luca.mantani@gmail.com>
2021-11-30 20:37:21 +0000

    Implemented first version of the visualiser class

---
+++ Visualiser.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Circle
+
+
+class Visualiser(object):
+
+    def __init__(self, physics, planets):
+        self.planets = planets
+        self.physics = physics
+
+    def run(self):
+
+        # Initialize figure
+        figure = plt.figure()
+        axes = figure.add_subplot(111, aspect='equal')
+        axes.set_xlim(-2, 2)
+        axes.set_ylim(-2, 2)
+
+        # Initialize graphics objects
+        self.circles = []
+        for planet in self.planets:
+            p = Circle((planet.position[0], planet.position[1]), 0.1)
+            p.set_visible(False)
+            axes.add_patch(p)
+            self.circles.append(p)
+
+        def animate(frameNumber):
+            # Without this, a copy of the figure will always be shown at its
+            # original position.
+            # Probably an easier way to do this, but the workaround is fine
+            if frameNumber == 1:
+                for c in self.circles:
+                    c.set_visible(True)
+
+            # Call the user updateFunc
+            self.physics.timestep(self.planets[0], self.planets[1])
+
+            for c, p in zip(self.circles, self.planets):
+                c.center = (p.position[0], p.position[1])
+
+            # if self.physics.collision(self.projectile, self.castle):
+            #     raise TypeError
+
+            return self.circles
+
+        ani = animation.FuncAnimation(figure,
+                                      animate,
+                                      frames=1000,
+                                      interval=25,
+                                      repeat=False,
+                                      blit=True)
+
+        plt.show()
